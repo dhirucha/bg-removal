34ecca57b9f16afe44a01157e05967aeace74986
Dheeraj chaubey <dheerajubecha@gmail.com>
2025-02-19 21:50:24 +0530

    add Navbar, Header, and Steps components; update App and Home pages for layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,18 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import BuyCredit from './pages/BuyCredit'
 import Result from './pages/Result'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <>
+    <div className='min-h-screen bg-slate-50'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/buy' element={<BuyCredit />} />
         <Route path='/result' element={<Result />} />
       </Routes>
-    </>
+    </div>
   )
 }
 
