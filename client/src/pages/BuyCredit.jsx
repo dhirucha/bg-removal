647b0010890343a23c01e20754807773b3ef238e
Dheeraj chaubey <dheerajubecha@gmail.com>
2025-02-21 21:42:12 +0530

    add Footer and Upload components; update Home, BuyCredit, and Result pages; remove unused bg_layer.png

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const BuyCredit = () => {
   return (
-    <div>BuyCredit</div>
+    <div className='min-h-[78vh] text-center pt-14 mb-10'>
+      <button>Our Plans</button>
+      <h1></h1>
+
+    </div>
   )
 }
 
