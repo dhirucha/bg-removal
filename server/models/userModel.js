f104777057baeb6a9ebe81ceea268f8af6593722
Dheeraj chaubey <dheerajubecha@gmail.com>
2025-03-03 19:41:22 +0530

    add user model with schema fields; implement clerk webhooks controller; configure Vercel deployment settings

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    
+    clerkId: {type:String, required:true, unique:true},
+    email: {type:String, required:true, unique:true},
+    photo: {type:String, required:true},
+    firstName: {type:String},
+    lastName: {type:String},
+    creditBalance: {type:Number, default:5},
+
 })
+
+const userModel = mongoose.model("User", userSchema) 
+|| mongoose.models.user;
+
+export default userModel;
