01f0c7f325f5d0f11874d0ccec45aa2cb8b09cd0
Dheeraj chaubey <dheerajubecha@gmail.com>
2025-03-15 20:40:37 +0530

    implement Razorpay payment integration for buying credits; add transaction model and update user routes

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets, plans } from '../assets/assets'
+import {AppContext} from '../context/AppContext'
+import { data, useNavigate } from 'react-router-dom'
+import { useAuth } from '@clerk/clerk-react'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const BuyCredit = () => {
+
+  const { backendUrl, loadCreditsData } = useContext(AppContext)
+
+  const navigate = useNavigate()
+
+  const { getToken } = useAuth()
+
+  const initPay = async (order) => {
+
+    const options = {
+      key : import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency,
+      name: 'Credits Payment',
+      description:"Credits payment",
+      order_id: order.id,
+      receipt: order.receipt,
+      handler: async (response) => {
+        console.log(response)
+      }
+    }
+
+    const rzp = new window.Razorpay(options)
+    rzp.open()
+
+  }
+
+  const paymentRazorpay = async (planId) =>{
+    try {
+
+      const token = await getToken()
+      const { data } = await axios.post(backendUrl + '/api/user/pay-razor', {planId},{headers:{token}})
+      if(data.success){
+        initPay(data.order)
+      }
+      
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message )
+    }
+  }
+
   return (
     <div className='min-h-[78vh] text-center pt-14 mb-10'>
       <button className='border border-gray-400 px-10 py-2 rounded-full mb-6'>Our Plans</button>
@@ -15,7 +62,7 @@ const BuyCredit = () => {
             <p className='mt-6'>
               <span className='text-3xl font-medium'>${item.price}</span>/{item.credits} credits
             </p>
-            <button className='w-full bg-gray-800 text-white mt-8 text-sm rounde-md py-2.5 min-w-52 cursor-pointer'>Purchase</button>
+            <button onClick={()=> paymentRazorpay(item.id)} className='w-full bg-gray-800 text-white mt-8 text-sm rounde-md py-2.5 min-w-52 cursor-pointer'>Purchase</button>
           </div>
         ))}
       </div>
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,7 @@
 import {Webhook} from 'svix'
 import userModel from '../models/userModel.js';
+import razorpay from 'razorpay'
+
 //Api controller function to manage clerk user with database
 //http://localhost:3000/api/user/webhooks
 
@@ -86,7 +88,83 @@ const userCredits = async (req, res) => {
     }
 }
 
+// gateway initialize
+const razorpayInstance = new razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+})
+
+
+//api to make payment for credits
+const paymentRazorpay = async (req,res) => {
+    try {
+
+        const { clerkId, planId } = req.body
+
+        const userData = await userModel.findOne({ clerkId })
+
+        if (!userData || !planId) {
+            return res.json({ success: false, message:'Invalid Credentials' })
+        }
+
+        let credits, plan, amount , date
+
+        switch (planId) {
+            case 'Basic':
+                plan = 'Basic'
+                credits = 100,
+                amount = 10
+                break;
+            
+            case 'Advanced':
+                 plan = 'Advanced'
+                 credits = 500,
+                 amount = 50
+                 break;  
+
+            case 'Business':
+                plan = 'Business'
+                credits = 500,
+                amount = 50
+                break;          
+        
+            default:
+                break;
+        }
+
+        data = Date.now()
+
+        //creating transaction
+        const transactionData = {
+            clerkId,
+            plan,
+            amount,
+            credits,
+            date
+        }
+
+        const newTransaction = await transactionModel.create(transactionData)
+
+        const options = {
+            amount: amount * 100,
+            currency: process.env.CURRENCY,
+            receipt: newTransaction._id
+
+        }
+
+        await razorpayInstance.orders.create(options,(error,order) =>{
+            if(error){
+                return res.json({success:false,message:error})
+            }
+            res.json({success:true,order})
+        })
+        
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false, message: error.message})
+    }
+}
 
 
 
-export {clerkWebhooks, userCredits}
+export {clerkWebhooks, userCredits, paymentRazorpay}
